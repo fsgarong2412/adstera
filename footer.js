4c46d58adb32242c11ac0865df10931ba86bfc39
Fahmi Permana <73010285+fsgarong2412@users.noreply.github.com>
2022-11-19 03:57:48 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,16 +1,15 @@
 document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
-document.write('<div style="text-align:center;display:block;max-width:300px;height:auto;overflow:hidden;margin:auto">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
 
 //Paste Iklan Disini
 	atOptions = {
-		'key' : '0e879a869721af9644e51c225cd631f1',
+		'key' : '7ab3fc02292382837aee16e237447158',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 90,
+		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/0e879a869721af9644e51c225cd631f1/invoke.js"></scr' + 'ipt>');
-// Batas Iklan
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/7ab3fc02292382837aee16e237447158/invoke.js"></scr' + 'ipt>');// Batas Iklan
 
 document.write('</div>');
 document.write('</div>');
